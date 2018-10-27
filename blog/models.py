b029f1d022c933cb700397b08487adcb6d3a29c7
emilews <emilews.ew@gmail.com>
2018-10-27 11:37:04 -0700

    Added categories, deleted author, description and dates

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,14 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Post(models.Model):
-	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+	GENDER_CHOICES = (
+        ('LIC', 'licra'),
+        ('CAM', 'camiseta'),
+    )
 	title = models.CharField(max_length=200)
 	text = models.TextField()
+	category = models.CharField(max_length=3, choices=GENDER_CHOICES)
 	price = models.IntegerField(null=True, blank=False, validators=[MinValueValidator(1),MaxValueValidator(999999)])
-	created_date = models.DateTimeField(default=timezone.now)
-	published_date = models.DateTimeField(blank=True, null=True)
 	image = models.ImageField(null=True, blank=False)
 
 	def publish(self):
