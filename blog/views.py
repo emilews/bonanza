f00521726af31eaccba8ca89ae38a37dfef18973
emilews <emilews.ew@gmail.com>
2018-10-29 14:15:47 -0700

    Se cambio la etiqueta form para que tuviera el default de html

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,8 @@ from django.views.generic import TemplateView
 
 
 def post_list(request):
+    cat = request.GET.get('categoria')
+    print(cat)
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
