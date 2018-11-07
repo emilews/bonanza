3bc4e4c426c60e90af75641b85cca1ba6450f44f
Emilio Wong <emilews.ew@gmail.com>
2018-11-06 20:12:20 -0700

    Added production

---
+++ build/precache-manifest.12e91b0dca1496bc1c32092d19e339f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fe304e3707d4d1e3e93",
+    "url": "/static/css/main.975d4413.chunk.css"
+  },
+  {
+    "revision": "7fe304e3707d4d1e3e93",
+    "url": "/static/js/main.7fe304e3.chunk.js"
+  },
+  {
+    "revision": "742d6ef9861ead008bb3",
+    "url": "/static/css/1.b6f03288.chunk.css"
+  },
+  {
+    "revision": "742d6ef9861ead008bb3",
+    "url": "/static/js/1.742d6ef9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac73766b515b10175d791ca7dd34bd85",
+    "url": "/static/media/4k.ac73766b.jpg"
+  },
+  {
+    "revision": "86d0d66520412b2c3f73d1f2bbabbf4f",
+    "url": "/index.html"
+  }
+];
